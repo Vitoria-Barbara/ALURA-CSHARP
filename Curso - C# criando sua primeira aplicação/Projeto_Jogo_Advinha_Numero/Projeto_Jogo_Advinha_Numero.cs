3385f573e40e82ef483eade52b1755bc900ba76f
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-12 13:26:55 -0300

    att code

--- Curso - C# criando sua primeira aplicação/Projeto_Jogo_Advinha_Numero/Projeto_Jogo_Advinha_Numero.cs
+++ Curso - C# criando sua primeira aplicação/Projeto_Jogo_Advinha_Numero/Projeto_Jogo_Advinha_Numero.cs
@@ -1,6 +1,6 @@
 ﻿Random aleatorio = new Random();
 int numeroSecreto = aleatorio.Next(1, 101);
-
+int numero = int.Parse( Console.ReadLine());
 do
 {
     Console.Write("Digite um número entre 1 e 100: ");
@@ -24,3 +24,33 @@ do
 
 Console.WriteLine("O jogo acabou. Você acertou o número secreto!");
 Console.ReadKey();// espera o usuário digitar algo para fechar o console.
+
+/*
+Random aleatorio = new Random();
+int numeroSecreto = aleatorio.Next(1, 101);
+
+int numero;
+
+Console.Write("Digite um número entre 1 e 100: ");
+numero = int.Parse(Console.ReadLine());
+
+while (numero != numeroSecreto)
+{
+    if (numero > numeroSecreto)
+    {
+        Console.WriteLine("É maior.");
+    }
+    else
+    {
+        Console.WriteLine("É menor.");
+    }
+
+    Console.Write("Digite um número entre 1 e 100: ");
+    numero = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Parabéns! Você acertou o número secreto.");
+
+Console.WriteLine("Pressione qualquer tecla para sair...");
+Console.ReadKey();
+*/
