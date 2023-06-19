3af363e29b48bdb4b3643921ca019f840164e9e8
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-19 17:51:18 -0300

    Atualização do código

--- Curso - C# criando sua primeira aplicação/Projeto_Screen_Sound/Projeto_Screen_Sound.cs
+++ Curso - C# criando sua primeira aplicação/Projeto_Screen_Sound/Projeto_Screen_Sound.cs
@@ -1,7 +1,10 @@
-﻿//Screen Sound
-string mensagemDeBoasVindas = "\nBoas Vindas ao Screen Sound";
+﻿//casesensitive
+//Screen Sound
 
-void ExibirMensagemDeBoasVindas()
+string mensagemDeBoasVindas = "\nBoas Vindas ao Screen Sound";
+List<string> ListaDasBandas = new List<string>();   
+
+void ExibirLogo()
 {
     //Verbatim Literal
     Console.WriteLine(@"
@@ -16,6 +19,7 @@ void ExibirMensagemDeBoasVindas()
 
 void ExibirOpçõesDoMenu()
 {
+    ExibirLogo();
     Console.WriteLine("\nDigite 1 para registrar uma banda");
     Console.WriteLine("Digite 2 para mostrar todas oa bandas");
     Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -28,7 +32,7 @@ void ExibirOpçõesDoMenu()
 
     switch (opcaoEscolhidaNumerica)
     {
-        case 1 : Console.WriteLine("Você escolheu a opção :" +opcaoEscolhidaNumerica);
+        case 1 : RegistrarBandas();
             break; 
         case 2 : Console.WriteLine("Você escolheu a opção :" + opcaoEscolhidaNumerica);
             break;
@@ -44,5 +48,18 @@ void ExibirOpçõesDoMenu()
 
 }
 
-ExibirMensagemDeBoasVindas();
+void RegistrarBandas() 
+{
+    Console.Clear();
+    Console.WriteLine("Registro de bandas:");
+    Console.Write("Digite o nome da banda que deseja registrar: ");
+    string nomeDaBanda = Console.ReadLine()!;//nao nulo
+    ListaDasBandas.Add(nomeDaBanda);
+    //interpolação de string 
+    Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso");
+    Thread.Sleep(2000);
+    Console.Clear();
+    ExibirOpçõesDoMenu();
+}
+
 ExibirOpçõesDoMenu();
