029acfe9a0d589b2a1e68e358f40fabceb0e195e
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-21 16:11:08 -0300

    minor changes

--- 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Music.cs
+++ 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Music.cs
@@ -1,58 +1,27 @@
-﻿class Musica //Creat a class
-{
-    //No C#, as propriedades ("regras"|"recursos") devem seguir a convenção Pascal Case>> Letra maiuscula no inicio
-    //(Convenção de nomes)
+﻿//ctrl + shift + a = Cria um novo item no projeto
+//Convenção Pascal Case>> Letra maiuscula no inicio
+//propriedade >> get|set
+//ATALHO >> cw + TAB = Write >> Console.WriteLine();
+//ctrl + R + R >> renomeia em all 
+////public string Nome - atributossssssss
 
-    //propriedade >> get|set
-
-    //ctrl + R + R >> renomeia em all lugares
-    public string Nome { get; set; } //propriedade permite ler(get) e escrever(set), em classes externas por ser public
-    //public string Nome //atributos
-    public string Artista { get; set; } //propriedade
-    public int Duracao { get; set; }//propriedade
-
-    //prop + TAB >>
-   /* public string DescricaoResumida 
-    {
-        get //propriedade só permite LEITURA (get), nega a escrita (set).
-        {
-            return $"A música {Nome} pertence a banda {Artista}";
-        }
-     }*/
-
-    public string DescricaoResumida => //GET (LAMBDA) >> arrow function (função de seta) >> (parametros) => expressao
-        $"A música {Nome} pertence a banda {Artista}"; 
-
-    //set - escreve 
-    //get - lê
-
-    public bool Disponivel { get; set; } //atributo | public - dispinivel para classes extrenas 
-                                         //privado - so acessa dentro das chaves 
-
-    /*
-    public void EscreveDisponivel(bool value)
-    {
-        disponivel = value;
-    }    
-
-    public bool LerDisponivel()
-    {
-        return disponivel;
-    }
-    */
-
-    public void ExibirFichaTecnica()//Creat a method - "função" (comportamento para os atributos)
+class Musica 
+{   
+    public string Nome { get; set; } //propriedade permite ler(get) e escrever(set), em classes externas (public)
+    public string Artista { get; set; } 
+    public int Duracao { get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida => $"A música {Nome} pertence a banda {Artista}"; //GET (LAMBDA) >> arrow function (função de seta)
+                                                                                        
+    public void ExibirFichaTecnica() //Creat a method - "função" (comportamento para os atributos)
     {
         //ATALHO >> cw + TAB = Write >> Console.WriteLine();
         Console.WriteLine($"Nome: {Nome}");
         Console.WriteLine($"Artista: {Artista}");
         Console.WriteLine($"Duração: {Duracao}");
-        if (Disponivel) 
-        {
+        if (Disponivel) {
             Console.WriteLine("Disponivel no plano.\n");
-        }
-        else
-        {
+        }else {
             Console.WriteLine("Adquira o plano Plus+\n");
         }
     }
