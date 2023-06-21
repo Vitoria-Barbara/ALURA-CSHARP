5fb618b0438c4f812af829862d91cb6ba63215e1
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-21 15:09:08 -0300

    minor changes

--- 2 - C# aplicando a Orientação a Objetos/Lambda_functions/Program.cs
+++ 2 - C# aplicando a Orientação a Objetos/Lambda_functions/Program.cs
@@ -18,3 +18,15 @@ foreach (int numero in numerosPares)
 
 List<int> numeros = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 */
+
+//No C#, uma função lambda pode ser aplicada em atributos, propriedades ou na forma como escrevemos funções. Abaixo, existe uma função que soma 2 valores inteiros e retorna o resultado da soma, como mostra o trecho de código a seguir:
+/*
+public int Somar(int a, int b)
+{
+    int resultado = a + b;
+    return resultado;
+}*/
+
+//Código refeito com este método Somar() usando uma função lambda retornando o resultado da operação.
+
+public int Somar(int a, int b) => a + b;
