b6453e25b040f55669fb8b64353482ce6a9aa0b1
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-21 08:57:31 -0300

    code increment

---
+++ 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Music.cs
@@ -0,0 +1,23 @@
+﻿class Musica //Creat a class
+{
+    public string nome; //atributos
+    public string artista; //atributos
+    public int duracao;//atributos
+    public bool disponivel; //atributos
+
+    public void ExibirFichaTecnica()//Creat a method - "função" (comportamento para os atributos)
+    {
+        //ATALHO >> cw + TAB = Write >> Console.WriteLine();
+        Console.WriteLine($"Nome: {nome}");
+        Console.WriteLine($"Artista: {artista}");
+        Console.WriteLine($"Duração: {duracao}");
+        if (disponivel) 
+        {
+            Console.WriteLine("Disponivel no plano.\n");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+\n");
+        }
+    }
+}
---
+++ 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Program.cs
@@ -0,0 +1,19 @@
+﻿Musica musica1 = new Musica(); // Criação de um objeto do tipo música
+musica1.nome = "Roxane";
+musica1.artista = "The Police";
+musica1.duracao = 254;    
+musica1.disponivel = true; 
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica2.artista = "U2";
+musica2.duracao = 367;
+musica2.disponivel = false;
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+/*
+Console.WriteLine($"Nome: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
+*/
