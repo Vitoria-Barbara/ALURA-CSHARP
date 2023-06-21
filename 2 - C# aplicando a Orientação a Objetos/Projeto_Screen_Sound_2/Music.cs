10c5fa36410a898013fa1b76821a6b15f860b05b
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-21 11:22:58 -0300

    Some improvements

--- 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Music.cs
+++ 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Music.cs
@@ -1,17 +1,53 @@
 ﻿class Musica //Creat a class
 {
-    public string nome; //atributos
-    public string artista; //atributos
-    public int duracao;//atributos
-    public bool disponivel; //atributos
+    //No C#, as propriedades ("regras"|"recursos") devem seguir a convenção Pascal Case>> Letra maiuscula no inicio
+    //(Convenção de nomes)
+
+    //propriedade >> get|set
+
+    //ctrl + R + R >> renomeia em all lugares
+    public string Nome { get; set; } //propriedade permite ler(get) e escrever(set), em classes externas por ser public
+    //public string Nome //atributos
+    public string Artista { get; set; } //propriedade
+    public int Duracao { get; set; }//propriedade
+
+    //prop + TAB >>
+   /* public string DescricaoResumida 
+    {
+        get //propriedade só permite LEITURA (get), nega a escrita (set).
+        {
+            return $"A música {Nome} pertence a banda {Artista}";
+        }
+     }*/
+
+    public string DescricaoResumida => //GET (LAMBDA) >> arrow function (função de seta)
+        $"A música {Nome} pertence a banda {Artista}"; 
+
+    //set - escreve 
+    //get - lê
+
+    public bool Disponivel { get; set; } //atributo | public - dispinivel para classes extrenas 
+                                         //privado - so acessa dentro das chaves 
+
+    /*
+    public void EscreveDisponivel(bool value)
+    {
+        disponivel = value;
+    }    
+
+    public bool LerDisponivel()
+    {
+        return disponivel;
+    }
+    */
 
     public void ExibirFichaTecnica()//Creat a method - "função" (comportamento para os atributos)
     {
         //ATALHO >> cw + TAB = Write >> Console.WriteLine();
-        Console.WriteLine($"Nome: {nome}");
-        Console.WriteLine($"Artista: {artista}");
-        Console.WriteLine($"Duração: {duracao}");
-        if (disponivel) 
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista}");
+        Console.WriteLine($"Duração: {Duracao}");
+        if (Disponivel) 
         {
             Console.WriteLine("Disponivel no plano.\n");
         }
---
+++ 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Projeto_Screen_Sound_2.cs
@@ -0,0 +1,26 @@
+﻿Musica musica1 = new Musica(); // Criação de um objeto do tipo música
+musica1.Nome = "Roxane";
+musica1.Artista = "The Police";
+musica1.Duracao = 254;
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
+Console.WriteLine(musica1.DescricaoResumida);
+//musica1.EscreveDisponivel(true);
+//Console.WriteLine(musica1.LerDisponivel());
+
+Musica musica2 = new Musica();
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
+musica2.Disponivel = false;
+Console.WriteLine(musica2.Disponivel);
+//musica2.EscreveDisponivel(true);
+//Console.WriteLine(musica1.LerDisponivel());
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
+
+/*
+Console.WriteLine($"Nome: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
+*/
