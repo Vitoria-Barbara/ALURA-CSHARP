0d95a769e36d8c8bf6f0fd6643d44b9f803ef306
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-22 06:54:49 -0300

    minor changes

---
+++ 2 - C# aplicando a Orientação a Objetos/Projeto_Screen_Sound_2/Class_Album.cs
@@ -0,0 +1,24 @@
+﻿class Album
+{
+    private List<Musica> musicas = new List<Musica>();
+
+    //prop 
+    public string Nome { get; set; }
+    public int DuracaoTotal => musicas.Sum(m => m.Duracao); //soma na lista Musica
+
+    //method >> behavior
+    public void AdicionarMusica(Musica musica)
+    {
+        musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}\n"); //alt + cima = leva para cima...
+        foreach (var musica in musicas) ///Para cada música na lista Musica
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"\nPara ouvir este álbum inteiro vôce precisa de: {DuracaoTotal}");
+    }
+}
