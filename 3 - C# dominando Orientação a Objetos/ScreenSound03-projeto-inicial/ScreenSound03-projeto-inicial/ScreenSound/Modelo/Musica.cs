b4b37f23d5ddc2c519851b83df885a6da7c91c17
VitoriAzevedo <97363737+VitoriAzevedo@users.noreply.github.com>
2023-06-24 16:34:37 -0300

    namespace

--- 3 - C# dominando Orientação a Objetos/ScreenSound03-projeto-inicial/ScreenSound03-projeto-inicial/ScreenSound/Modelo/Musica.cs
+++ 3 - C# dominando Orientação a Objetos/ScreenSound03-projeto-inicial/ScreenSound03-projeto-inicial/ScreenSound/Modelo/Musica.cs
@@ -1,4 +1,5 @@
-﻿class Musica
+﻿namespace Screen_Sound_3.Modelos;
+class Musica
 {
     public Musica(Banda artista, string nome)
     {
--- 3 - C# dominando Orientação a Objetos/ScreenSound03-projeto-inicial/ScreenSound03-projeto-inicial/ScreenSound/Program.cs
+++ 3 - C# dominando Orientação a Objetos/ScreenSound03-projeto-inicial/ScreenSound03-projeto-inicial/ScreenSound/Program.cs
@@ -1,4 +1,7 @@
-﻿Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
+﻿Projeto_Screen_Sound_3.Modelos.Album  album = new Projeto_Screen_Sound_3.Modelos.Album("Albumm");
+Projeto_Screen_Sound_3.Modelos.Banda banda = new Projeto_Screen_Sound_3.Modelos.Banda("Bandaa");
+
+Dictionary<string, List<int>> bandasRegistradas = new Dictionary<string, List<int>>();
 bandasRegistradas.Add("Linkin Park", new List<int> { 10, 8, 6 });
 bandasRegistradas.Add("The Beatles", new List<int>());
 
